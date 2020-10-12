848af7768146c644e86d6c025060b3548fd3f397
Erin A <eaannis@umich.edu>
2020-10-12 16:23:57 -0400

    First hw5_git.py file

---
+++ hw5_git.py
@@ -0,0 +1,7 @@
+list1 = ['book', 'tome', 'manuscript']
+list2 = ['parchment', 'paper']
+
+for i in list1:
+    print('I am a book.')
+    print('I am a tome.')
+    print('I am manscript made of either', list2[0], 'or', list2[1])
